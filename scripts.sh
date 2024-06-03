ca57ba4f1f78f7908bdcb082c5014f0dbf896e87
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2024-06-02 19:44:12 -0500

    :sparkles: add event rule to execute with a cron expression

--- scripts.sh
+++ scripts.sh
@@ -1,23 +1,45 @@
 set -e
+PROJECT_NAME="ecs-example"
 
 action=$1
 
 if [ "$action" = "setup-infra" ]; then
     aws cloudformation create-stack \
-        --stack-name "example" \
+        --stack-name $PROJECT_NAME-stack \
+        --template-body file://cloudformation.yaml \
+        --parameters ParameterKey=LambdaName,ParameterValue="$LAMBDA_NAME" ParameterKey=CronExecutionExpression,ParameterValue="$CRON_EXECUTION_EXPRESSION" \
+        --capabilities CAPABILITY_NAMED_IAM \
+        | cat
+
+elif [ "$action" = "update-infra" ]; then
+    aws cloudformation update-stack \
+        --stack-name $PROJECT_NAME-stack \
         --template-body file://cloudformation.yaml \
         --capabilities CAPABILITY_NAMED_IAM \
         | cat
 
+elif [ "$action" = "delete-ecr-img" ]; then
+    aws ecr batch-delete-image \
+        --repository-name "$PROJECT_NAME-repo" \
+        --image-ids "imageTag=latest" \
+        | cat
+
+elif [ "$action" = "delete-infra" ]; then
+    sh scripts.sh delete-ecr-img
+
+    aws cloudformation delete-stack \
+        --stack-name $PROJECT_NAME-stack \
+        | cat
+
 elif [ "$action" = "repo-uri" ]; then
     aws cloudformation describe-stacks \
-        --stack-name example \
+        --stack-name $PROJECT_NAME-stack \
         --query "Stacks[0].Outputs[?OutputKey=='RepositoryUri'].OutputValue | [0]" \
         --output text \
         | cat
 
 elif [ "$action" = "build" ]; then
-    docker build --no-cache -t ecs-template .
+    docker build --no-cache -t $PROJECT_NAME .
 
 elif [ "$action" = "deploy" ]; then
     sh scripts.sh build
@@ -30,10 +52,12 @@ elif [ "$action" = "deploy" ]; then
 
     aws ecr get-login-password --region us-east-1 \
         | docker login --username AWS --password-stdin $REPO_SERVER
-    docker tag ecs-template:latest $REPO_URI:latest
+
+    # TODO create tag with last commit hash
+    docker tag $PROJECT_NAME:latest $REPO_URI:latest
     docker push $REPO_URI
 
 elif [ "$action" = "run" ]; then
-    docker run --rm ecs-template
+    docker run --rm $PROJECT_NAME
 
 fi
