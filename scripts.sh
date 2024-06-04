f1e82fd2f0e63b939bc0c445b0b050f822e839d6
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2024-06-04 10:48:29 -0500

    :sparkles: tag images with latest commit hash

--- scripts.sh
+++ scripts.sh
@@ -2,9 +2,27 @@ set -e
 
 PROJECT_NAME="ecs-example"
 CRON_EXECUTION_EXPRESSION="*/15 * * * ? *"
-SUBNET_IDS="subnet-00000000\,subnet-11111111"
+SUBNET_IDS="subnet-0960a7245ae53828e\,subnet-0bc8639c51c251489\,subnet-05de9ebbb19c04a7b\,subnet-0041d932d16af8ae6\,subnet-06a9d5ebfeeb6274d\,subnet-0b347f25cc5e1a05a"
 
 action=$1
+
+function latest_commit() {
+    echo "$(git log --oneline -1 | awk '{print $1}')"
+}
+
+function log() {
+  GREEN='\033[0;32m'
+  YELLOW="\033[0;33m"
+  COLOR_OFF="\033[0m"
+
+  if [ "$2" = "warn" ]; then
+    color="$YELLOW"
+  else
+    color="$GREEN"
+  fi
+
+  printf "\n${color}$1${COLOR_OFF}\n"
+}
 
 if [ "$action" = "setup-infra" ]; then
     aws cloudformation create-stack \
@@ -49,7 +67,11 @@ elif [ "$action" = "repo-uri" ]; then
         | cat
 
 elif [ "$action" = "build" ]; then
-    docker build --no-cache -t $PROJECT_NAME .
+    if [ $2 == "no-cache" ]; then
+        docker build --no-cache -t $PROJECT_NAME .
+    else
+        docker build -t $PROJECT_NAME .
+    fi
 
 elif [ "$action" = "default-subnets" ]; then
     vpc=$(
@@ -59,7 +81,7 @@ elif [ "$action" = "default-subnets" ]; then
         --output text \
     )
 
-    echo "default vpc = $vpc"
+    log "default vpc = $vpc"
 
     # replace tabs by \, because it's the valid way to pass a 
     # param value in cloudformation cli for 'setup-infra' command
@@ -70,25 +92,28 @@ elif [ "$action" = "default-subnets" ]; then
         | sed "s/\t/\\\,/g"
 
 elif [ "$action" = "deploy" ]; then
-    sh scripts.sh build
+    sh scripts.sh build 
 
     REPO_URI=$(sh scripts.sh repo-uri)
     REPO_SERVER=$(echo $REPO_URI | awk -F / '{print $1}')
 
-    echo "REPO_URI: $REPO_URI"
-    echo "REPO_SERVER: $REPO_SERVER"
+    log "REPO_URI: $REPO_URI"
+    log "REPO_SERVER: $REPO_SERVER"
 
     aws ecr get-login-password --region us-east-1 \
         | docker login --username AWS --password-stdin $REPO_SERVER
 
-    # TODO create tag with last commit hash
+    commit=$(latest_commit)
+
     docker tag $PROJECT_NAME:latest $REPO_URI:latest
-    docker push $REPO_URI
+    docker tag $PROJECT_NAME:latest "$REPO_URI:$commit"
+    docker push "$REPO_URI:latest"
+    docker push "$REPO_URI:$commit"
 
 elif [ "$action" = "run" ]; then
     docker run --rm $PROJECT_NAME
 
 else 
-    echo "action '$action' not found"
+    log "action '$action' not found" "warn"
     exit 1
 fi
